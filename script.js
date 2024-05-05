d03c95b39d0c9879c3d733b4b23d5efaf7e3f73d
PaPaBen17 <145366453+PaPaBen17@users.noreply.github.com>
2024-05-05 17:19:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const operation = document.getElementById("operation").getAttribute("data-operation");
+    let result;
+
+    switch (operation) {
+        case "add":
+            result = num1 + num2;
+            break;
+        case "subtract":
+            result = num1 - num2;
+            break;
+        case "multiply":
+            result = num1 * num2;
+            break;
+        case "divide":
+            if (num2 === 0) {
+                result = "Division by zero is not allowed!";
+            } else {
+                result = num1 / num2;
+            }
+            break;
+        default:
+            result = "Invalid operation!";
+    }
+
+    document.getElementById("result").textContent = "Result: " + result;
+}
+
+function setOperation(operation) {
+    document.getElementById("operation").setAttribute("data-operation", operation);
+    calculate();
+}
+
